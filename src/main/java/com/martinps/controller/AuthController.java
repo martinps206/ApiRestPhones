57bb96b7cf03ed4864afd4527c354163e21ceeb5
martinps206 <martinps_cc@hotmail.com>
2023-12-27 23:51:39 -0300

    Metodos controller API

--- src/main/java/com/martinps/controller/AuthController.java
+++ src/main/java/com/martinps/controller/AuthController.java
@@ -1,15 +1,16 @@
 package com.martinps.controller;
 
 import com.martinps.dto.UserDTO;
+import com.martinps.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import com.martinps.service.UserService;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class AuthController {
@@ -19,13 +20,42 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody @Valid UserDTO userDTO){
-        if (userDTO.getEmail() == null) {
-            System.out.println("no hay datos----->");
-        }
-
         UserDTO createdUser = userService.register(userDTO);
-
         return ResponseEntity.created(URI.create("/user/" + createdUser.getId())).body(createdUser);
-
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        List<UserDTO> userDTOS = userService.getAllUsers();
+
+        if (!userDTOS.isEmpty()) {
+            return new ResponseEntity<>(userDTOS, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String id){
+        UserDTO userDTO = userService.findUserById(id);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<String> deletedUser(@PathVariable String id){
+        try {
+            userService.deletedUser(id);
+            return ResponseEntity.ok("Deleted user");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed deleted user" + e.getMessage());
+        }
+    }
+
+    @PutMapping("user/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody UserDTO userDTO){
+        UserDTO userUpdate = userService.updateUser(id, userDTO);
+        //return ResponseEntity.ok(userUpdate);
+        return new ResponseEntity<>(userUpdate, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/martinps/service/UserService.java
+++ src/main/java/com/martinps/service/UserService.java
@@ -1,6 +1,5 @@
 package com.martinps.service;
 
-import com.martinps.dto.PhoneDTO;
 import com.martinps.dto.UserDTO;
 import com.martinps.entity.Phone;
 import com.martinps.entity.User;
@@ -11,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class UserService {
@@ -25,15 +22,64 @@ public class UserService {
 
     public UserDTO register(UserDTO userDTO){
         Optional<User> optionalUser = userRepository.findByEmail(userDTO.getEmail());
-
         if (optionalUser.isPresent()){
-            throw new AppException("Login already exists", HttpStatus.BAD_REQUEST);
+            throw new AppException("User already exists", HttpStatus.BAD_REQUEST);
+        }
+        System.out.println("userDTO --> " + userDTO.toString());
+        User user = userMapper.signUpToUser(userDTO);
+
+        if (userDTO.getPhones() != null && !userDTO.getPhones().isEmpty()){
+            Set<Phone> phones = new HashSet<>();
+            for (Phone phoneDTO : userDTO.getPhones()){
+                Phone phone = new Phone();
+                phone.setNumber(phoneDTO.getNumber());
+                phone.setCityCode(phoneDTO.getCityCode());
+                phone.setCountryCode(phoneDTO.getCountryCode());
+                phone.setUser(user);
+                phones.add(phone);
+            }
+            user.setPhones(phones);
+        }
+        User savedUser = userRepository.save(user);
+        return userMapper.toUserDto(savedUser);
+    }
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserDTO> userDTOs = new ArrayList<>();
+        for (User user : users) {
+            userDTOs.add(userMapper.toUserDto(user));
+        }
+        return userDTOs;
+    }
+
+    public UserDTO findUserById(String id){
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userMapper.toUserDto(userOptional.get());
+
+        }else{
+            throw new AppException("User doesn't exist", HttpStatus.BAD_REQUEST);
         }
 
-        System.out.println("userDTO --> " + userDTO.toString());
+    }
+
+    public void deletedUser(String id){
+        userRepository.deleteById(id);
+    }
+
+    public UserDTO updateUser(String id, UserDTO userDTO){
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new AppException("User doesn't exist", HttpStatus.BAD_REQUEST);
+        }
 
         User user = userMapper.signUpToUser(userDTO);
-        System.out.println("user --> " + user.toString());
+        userOptional.get().setId(user.getId());
+        userOptional.get().setName(user.getName());
+        userOptional.get().setEmail(user.getEmail());
+        userOptional.get().setPassword(user.getPassword());
+
 
         if (userDTO.getPhones() != null && !userDTO.getPhones().isEmpty()){
             Set<Phone> phones = new HashSet<>();
@@ -48,13 +94,9 @@ public class UserService {
             user.setPhones(phones);
         }
 
-        User savedUser = userRepository.save(user);
-        System.out.println("savedUser --> " + savedUser.toString());
-
+        User savedUser = userRepository.save(userOptional.get());
         return userMapper.toUserDto(savedUser);
     }
-
-
 
 
 }
